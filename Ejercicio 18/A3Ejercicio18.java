35eddc7324104a8fb2b43baaf0e2efb3f22b600a
LucasOsorno <150816146+LucasOsorno@users.noreply.github.com>
2025-01-13 20:58:17 -0500

    Create A3Ejercicio18.java

---
+++ Ejercicio 18/A3Ejercicio18.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg3aejercicio18;
+
+public class A3Ejercicio18 {
+
+    public static void main(String[] args) {
+       VentanaPrincipal ventana  = new VentanaPrincipal();
+       ventana.setVisible(true);   
+    }  
+}
