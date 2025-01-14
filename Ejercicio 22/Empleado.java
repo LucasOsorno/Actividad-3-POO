53a0e7af883d28a3cafcfc008a9fcd6c6b523b56
LucasOsorno <150816146+LucasOsorno@users.noreply.github.com>
2025-01-13 21:09:40 -0500

    Create Empleado.java

---
+++ Ejercicio 22/Empleado.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg3aejercicio22;
+
+public class Empleado {
+    public static double SalarioMensual(double Basico, double Horas){
+    double salario_mensual;
+    salario_mensual = Basico*Horas;
+    return salario_mensual;
+    }
+    
+    public static String Nombre(String nombre){
+        return nombre;
+    }
+}
+    
